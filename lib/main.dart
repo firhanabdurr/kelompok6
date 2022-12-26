207035c0f02329d83bf8b939caf2d9467dba612b
firhanabdurr <firhanabdurr@gmail.com>
2022-12-26 23:21:37 +0700

    failed connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
