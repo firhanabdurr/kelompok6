a02a7255f98031faf8a166a90311a5e87864e808
firhanabdurr <firhanabdurr@gmail.com>
2022-12-29 17:34:28 +0700

    masih error

--- lib/screens/note_editor.dart
+++ lib/screens/note_editor.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class NoteEditorScreen extends StatefulWidget {
-  const NoteEditorScreen({Key? key}) : super(key: key)
+  const NoteEditorScreen({Key? key}) : super(key: key);
 
   @override
   State<NoteEditorScreen> createState() => _NoteEditorScreenState();
@@ -21,16 +21,18 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppStyle.cardsColor[color_id],
-      appBar: AppBar(backgroundColor: AppStyle.cardsColor[color_id],
-      elevation: 0.0,
-      iconTheme: IconThemeData(color: Colors.black),
-      title: Text("Add new note", style: TextStyle(color: Colors.black),), 
+      appBar: AppBar(
+        backgroundColor: AppStyle.cardsColor[color_id],
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Add new note",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           TextField(
             controller: _titleController,
             decoration: InputDecoration(
@@ -39,9 +41,16 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
             ),
             style: AppStyle.mainTitle,
           ),
-          SizedBox(height: 8.0,),
-          Text(date, style: AppStyle.dateTitle,),
-          SizedBox(height: 28.0,),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            date,
+            style: AppStyle.dateTitle,
+          ),
+          SizedBox(
+            height: 28.0,
+          ),
           TextField(
             controller: _mainController,
             keyboardType: TextInputType.multiline,
@@ -56,16 +65,17 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: AppStyle.accentColor,
-        onPressed: ()async {
+        onPressed: () async {
           FirebaseFirestore.instance.collection("Notes").add({
-            "note_title" : _titleController.text,
+            "note_title": _titleController.text,
             "creation_date": date,
             "note_content": _mainController.text,
             "color_id": color_id
-          }).then((value){
+          }).then((value) {
             print(value.id);
             Navigator.pop(context);
-          }).catchError((error) => print("Failed to add new note due to $error"));
+          }).catchError(
+              (error) => print("Failed to add new note due to $error"));
         },
         child: Icon(Icons.save),
       ),
